ad4b46ccb541977f6db801cb2fa94a520488adae
Ayush Malik <ayushmalik397@gmail.com>
2019-03-03 22:50:55 +0530

    field edited

--- helloapp/howdy/send_sms.py
+++ helloapp/howdy/send_sms.py
@@ -1,13 +1,14 @@
 from twilio.rest import Client
 
-def send(number, message):
+def send(number, name):
+    text_to_send = "Hello! Mr./Mrs. "+name+" is coming to your appartment"
     account_sid = 'ACdaa6bc9c6e6a6ae060e3b8df52aa4eff'
     auth_token = '6295cc3f4f569d4e80a826dc01b627d0'
     client = Client(account_sid, auth_token)
 
     message = client.messages \
                     .create(
-                         body=message,
+                         body=text_to_send,
                          from_='+17755574512',
                          to=number
                      )
--- helloapp/howdy/views.py
+++ helloapp/howdy/views.py
@@ -13,16 +13,10 @@ class HomePageView(TemplateView):
     def post(self, request, **kwargs):
         number = request.POST['contact']
         print(number)
-        message = request.POST['text']
-        print(message)
-        send(number, message)
-        #message = request.POST['text']
+        name = request.POST['gname']
+        print(name)
+        #send(number, name)
         return render(request, 'index.html', context=None)
-
-    # def post(self, request, **kwargs):
-    #     message = request.POST['text']
-    #     print(message)
-    #     return render(request, 'index.html', context=None)
 
     def get(self, request, **kwargs):
         return render(request, 'index.html', context=None)
